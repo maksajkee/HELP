5fd5a8da98aec93b0e780617b156755f7e4919b7
User <User@DESKTOP-FTPG4U2>
2022-05-14 17:45:50 +0300

    Добавьте файлы проекта.

---
+++ HELP/AuthWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HELP
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Auth_Click(object sender, RoutedEventArgs e)
+        {
+           
+            
+
+            string login = textBoxlogin.Text;
+            string pass = passbox.Password;
+
+            if (login.Length < 5)
+            {
+                textBoxlogin.ToolTip = "Введено не Корректно";
+                textBoxlogin.Background = Brushes.DarkRed;
+
+            }
+            else if (pass.Length < 5)
+            {
+                passbox.ToolTip = "Введено не Корректно";
+                passbox.Background = Brushes.DarkRed;
+
+            }
+            else
+            {
+                textBoxlogin.ToolTip = "";
+                textBoxlogin.Background = Brushes.Transparent;
+                passbox.ToolTip = "";
+                passbox.Background = Brushes.Transparent;
+
+               
+
+             
+                MessageBox.Show("Всё хорошо");
+                UserPageWindow userPageWindow = new UserPageWindow();
+                userPageWindow.Show();
+                Hide();
+            }
+        }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Hide();
+        }
+    }
+}
---
+++ HELP/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HELP
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxlogin.Text;
+            string pass = passbox.Password;
+
+            if (login.Length < 5)
+            {
+                textBoxlogin.ToolTip = "Введено не Корректно";
+                textBoxlogin.Background = Brushes.DarkRed;
+
+            }else if(pass.Length < 5)
+            {
+                passbox.ToolTip = "Введено не Корректно";
+                passbox.Background = Brushes.DarkRed;
+
+            }
+            else
+            {
+                textBoxlogin.ToolTip = "";
+                textBoxlogin.Background = Brushes.Transparent;
+                passbox.ToolTip = "";
+                passbox.Background = Brushes.Transparent;
+
+                MessageBox.Show("Всё хорошо");
+            }
+            
+            
+        }
+
+        private void Button_Window_Auth_Click(object sender, RoutedEventArgs e)
+        {
+            AuthWindow authWindow = new AuthWindow();
+            authWindow.Show();
+            this.Hide();
+        }
+    }
+}
